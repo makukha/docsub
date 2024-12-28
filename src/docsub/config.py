f2dc3e7d72f5496652827806fba7aab9e90df945
Michael Makukha <m.makukha@gmail.com>
2024-12-28 18:43:07 +0200

    Make .docsub.toml config file optional

--- src/docsub/config.py
+++ src/docsub/config.py
@@ -1,6 +1,9 @@
+from pathlib import Path
 import tomllib
 
 
-def load_config(path: str = '.docsub.toml') -> dict:
-    with open(path, 'rb') as f:
-        return tomllib.load(f)
+def load_config(path: Path = Path('.docsub.toml')) -> dict:
+    if path.exists():
+        return tomllib.loads(path.read_text())
+    else:
+        return {}
