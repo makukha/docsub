1c3b8569aa4e774006745530acf300bcce0af3d1
Michael Makukha <m.makukha@gmail.com>
2025-01-18 07:38:30 +0200

    Pass local x commands to cli

--- src/docsub/__init__.py
+++ src/docsub/__init__.py
@@ -1,1 +1,12 @@
+import rich_click as click
+from rich_click import pass_obj as pass_env
+
+from .environment import Environment, pass_env
+
 __version__ = '0.7.1'
+
+__all__ = [
+    'Environment',
+    'click',
+    'pass_env',
+]
--- src/docsub/__main__.py
+++ src/docsub/__main__.py
@@ -1,14 +1,9 @@
-import importlib
 import json
 from pathlib import Path
-import shlex
-from typing import Annotated
 
 import rich_click as click
 
 from . import __version__
-from .__base__ import Location
-# from .commands import XCommand
 from .environment import Environment
 from .process import process_paths
 
@@ -17,11 +12,11 @@ from .process import process_paths
 @click.version_option(__version__, prog_name='docsub')
 @click.option('-c', '--config-file', type=Path)
 @click.option('-l', '--local-dir', type=Path)
-@click.option( '--exec-work-dir', type=Path)
-@click.option( '--exec-env-vars', type=str)
-@click.option( '--help-env-vars', type=str)
-@click.option( '--include-base-dir', type=Path)
-@click.option( '-x', '--x-docsubfile', type=Path)
+@click.option( '--cmd-exec-work-dir', type=Path)
+@click.option( '--cmd-exec-env-vars', type=str)
+@click.option( '--cmd-help-env-vars', type=str)
+@click.option( '--cmd-include-base-dir', type=Path)
+@click.option( '-x', '--cmd-x-docsubfile', type=Path)
 @click.pass_context
 def app(
     ctx: click.Context,
@@ -29,11 +24,11 @@ def app(
     # root settings
     local_dir: Path | None = None,
     # commands settings
-    exec_work_dir: Path | None = None,
-    exec_env_vars: str | None = None,
-    help_env_vars: str | None = None,
-    include_base_dir: Path | None = None,
-    x_docsubfile: Path | None = None,
+    cmd_exec_work_dir: Path | None = None,
+    cmd_exec_env_vars: str | None = None,
+    cmd_help_env_vars: str | None = None,
+    cmd_include_base_dir: Path | None = None,
+    cmd_x_docsubfile: Path | None = None,
 
 ):
     def maybe_json_loads(value: str | None) -> dict | None:
@@ -41,14 +36,15 @@ def app(
             return json.loads(value)
 
     ctx.obj = Environment.from_config_file(
+        ctx=ctx,
         config_file=config_file,
         options={
             'local_dir': local_dir,
-            'cmd.exec.work_dir': exec_work_dir,
-            'cmd.exec.env_vars': maybe_json_loads(exec_env_vars),
-            'cmd.help.env_vars': maybe_json_loads(help_env_vars),
-            'cmd.include.base_dir': include_base_dir,
-            'cmd.x.docsubfile': x_docsubfile,
+            'cmd.exec.work_dir': cmd_exec_work_dir,
+            'cmd.exec.env_vars': maybe_json_loads(cmd_exec_env_vars),
+            'cmd.help.env_vars': maybe_json_loads(cmd_help_env_vars),
+            'cmd.include.base_dir': cmd_include_base_dir,
+            'cmd.x.docsubfile': cmd_x_docsubfile,
         },
     )
 
@@ -69,6 +65,23 @@ def apply(
     place it after that file.
     """
     process_paths(files, in_place=in_place, env=ctx.obj)
+
+
+class XGroup(click.RichMultiCommand):
+    def list_commands(self, ctx):
+        env: Environment = ctx.obj
+        return env.x_group.list_commands(ctx)
+
+    def get_command(self, ctx, name):
+        env: Environment = ctx.obj
+        return env.x_group.get_command(ctx, name)
+
+
+@app.command(cls=XGroup)
+def x():
+    """
+    Project-local commands.
+    """
 
 
 if __name__ == '__main__':
